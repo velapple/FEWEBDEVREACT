9cd0136223af7424d782c898c54da687f02d1f60
Velapple <velloryapple@gmail.com>
2020-11-18 01:02:28 +0100

    add react router to change web app to web site

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import RenderMenuItem from './MenuComponent';
+import Home from './HomeComponent';
+import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent'
 import {DISHES} from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
+import {Switch,Route,Redirect} from 'react-router-dom';
 
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     }
     this.onDishSelect = this.onDishSelect.bind(this)
   }
@@ -23,15 +24,19 @@ class Main extends Component {
     }
 
   render () {
+      const HomePage = () => {
+          return(
+              <Home />
+          )
+      }
     return(
       <div>
         <Header />
-        <div>
-        <RenderMenuItem dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        {this.state.selectedDish !== null && 
-            <DishDetail 
-            dish={this.state.dishes.filter(dish => dish.id === this.state.selectedDish)[0]}/>}
-        </div>
+        <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> } />
+            <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     )}
